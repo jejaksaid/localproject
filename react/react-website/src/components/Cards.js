e8f5e3cd8e3f10a6daec7f9cb9acab524983d5c4
jejaksaid <said@fastlearningcenter.com>
2021-01-22 01:32:03 +0700

    2201

--- react/react-website/src/components/Cards.js
+++ react/react-website/src/components/Cards.js
@@ -17,11 +17,38 @@ function Cards() {
                         />
 
                         <CardItem 
-                        src="images/img-2.jpg" 
-                        text="Travel through the Island of Bali in a Private Cruise"
-                        label="Luxury" 
-                        path="/services" />
+                            src="images/img-2.jpg" 
+                            text="Travel through the Island of Bali in a Private Cruise"
+                            label="Luxury" 
+                            path="/services" 
+                        />
+
+                        
                     </ul>
+
+                    <ul className="cards__items">
+                        <CardItem 
+                            src="images/img-3.jpg" 
+                            text="Set Sail in the Atlantic Ocean visiting Uncharted Waters" 
+                            label="Mystery" 
+                            path="/services"
+                        />
+
+                        <CardItem 
+                            src="images/img-4.jpg" 
+                            text="Experience Footbal on Top of the Himalayan Mountains"
+                            label="Adventure" 
+                            path="/services" 
+                        />
+
+                        <CardItem 
+                            src="images/img-8.jpg" 
+                            text="Raid through the Sahara Desert on a guided camel tour"
+                            label="Adrenaline" 
+                            path="/services" 
+                        />
+                    </ul>
+
                 </div>
             </div>
         </div>
