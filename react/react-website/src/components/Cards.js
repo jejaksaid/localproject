93d12d91157a0e5c9dc0fa7e61ab6250b0f22726
jejaksaid <said@fastlearningcenter.com>
2021-01-21 17:00:52 +0700

    2101

--- react/react-website/src/components/Cards.js
+++ react/react-website/src/components/Cards.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards() {
     return (
@@ -7,7 +9,18 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <Cards />
+                        <CardItem 
+                            src="images/img-9.jpg" 
+                            text="Explore the hidden waterfall deep inside the Amazon Jungle" 
+                            label="Adventure" 
+                            path="/services"
+                        />
+
+                        <CardItem 
+                        src="images/img-2.jpg" 
+                        text="Travel through the Island of Bali in a Private Cruise"
+                        label="Luxury" 
+                        path="/services" />
                     </ul>
                 </div>
             </div>
