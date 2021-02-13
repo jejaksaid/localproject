7b88d5370e3f28d6553848fa2299101cd4421bb5
jejaksaid <said@fastlearningcenter.com>
2021-02-14 01:06:59 +0700

    adding Link

--- react/jejaksaid/src/App.js
+++ react/jejaksaid/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    
+    <>
+    <Router>
+      <Navbar />
+    </Router>
+    </>
   );
 }
 
--- react/jejaksaid/src/components/Navbar.js
+++ react/jejaksaid/src/components/Navbar.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
+    
+    
+    
     return (
         <>
-        
+        <nav className='navbar'>
+            <div className="navbar-container">
+                <Link to='/' className='navbar-logo'>
+                <b>Jejak</b>Said
+                <i className="fab-fa-typo3"></i>
+                </Link>
+            </div>
+        </nav>
         </>
     )
 }
