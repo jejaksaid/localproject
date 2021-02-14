8cedd5701b33b609be43d81f30565fcbb0204446
jejaksaid <said@fastlearningcenter.com>
2021-02-14 21:55:08 +0700

    navigation

--- react/jejaksaid/src/components/Navbar.js
+++ react/jejaksaid/src/components/Navbar.js
@@ -11,9 +11,24 @@ function Navbar() {
         <nav className='navbar'>
             <div className="navbar-container">
                 <Link to='/' className='navbar-logo'>
-                <b>Jejak</b>Said
+                <b>Jejak </b> Said
                 <i className="fab-fa-typo3"></i>
                 </Link>
+                <div className="menu-icon">
+                    <i className={click ? "fas fa-times" : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : "nav-menu active"}>
+                    <li className="nav-item">
+                        <Link to='/' className='nav-links'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/portfolio' className='nav-links'>
+
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
         </>
