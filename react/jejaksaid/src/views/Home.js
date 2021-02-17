94ee444cc44d84de2270251e87833c737eaa5a44
jejaksaid <said@fastlearningcenter.com>
2021-02-17 22:11:43 +0700

    adding about component

--- react/jejaksaid/src/views/Home.js
+++ react/jejaksaid/src/views/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import AppHero from '../components/home/Hero'
+import AppHero from '../components/home/Hero';
+import AppAbout from '../components/home/About';
 
 function AppHome() {
     return (
         <div className='main'>
             <AppHero />
+            <AppAbout />
         </div>    
     );
 }
