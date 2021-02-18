fa8ca2206b44534647d26a4f6e67e2412fac114e
jejaksaid <said@fastlearningcenter.com>
2021-02-19 06:40:14 +0700

    adding something new

--- react/jejaksaid/src/App.js
+++ react/jejaksaid/src/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'antd/dist/antd.css';
 
-import AppHeader from './components/common/Header';
-import AppHome from './components/pages/Home';
-import About from './components/pages/About';
+import AppHeader from './components/common/header';
+import AppFooter from './components/common/footer';
+import AppHome from './views/home';
 
 import { Layout } from 'antd';
-const { Header, Content } = Layout;
-
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <>
-    <Router>
-      <Layout className="mainLayout">
-        <Header>
-          <AppHeader />
-          
-        </Header>
-        <Content>
-          <AppHome />
-        </Content>
-      </Layout>
-    </Router>
-    </>
+    <Layout className="mainLayout">
+      <Header>
+        <AppHeader/>
+      </Header>
+      <Content>
+        <AppHome/>
+      </Content>
+      <Footer>
+        <AppFooter/>  
+      </Footer>      
+    </Layout>
   );
 }
 
