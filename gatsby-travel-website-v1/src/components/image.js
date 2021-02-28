49c95c4c235c7ee4ab03a16ccb7bc37ef50e0eae
jejaksaid <said@fastlearningcenter.com>
2021-02-28 23:29:07 +0700

    updating HeroSection.css

--- gatsby-travel-website-v1/src/components/image.js
+++ gatsby-travel-website-v1/src/components/image.js
@@ -1,9 +1,41 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
+import React from 'react'
+import Img from 'gatsby-image'
+
 
 const Image = () => {
-  
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(filter: {extension: {regex: "/(jpg)|(jpeg)|(png)/"}, name: {nin: ["background", "background2"]}}) {
+        edges {
+          node {
+            base 
+            childImageSharp {
+              fluid(maxHeight: 600, maxWidth: 600) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)  
+
+  return (
+    <div className="image-container">
+      <h1>View Our Destinations</h1>
+      <div className="image-grid">
+        {data.allFile.edges.map((image, key) => (
+          <Img
+            key={key}
+            className='image-item'
+            fluid={image.node.childImageSharp.fluid}
+            alt={image.node.base.split('.')[0]}
+            />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Image
