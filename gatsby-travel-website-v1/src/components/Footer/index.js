63e3116cf5b379bd2cfe54c7c272bb8ef974f91e
jejaksaid <said@fastlearningcenter.com>
2021-03-01 08:43:18 +0700

    editing index Footer

--- gatsby-travel-website-v1/src/components/Footer/index.js
+++ gatsby-travel-website-v1/src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from 'react-icons/fa'
+import { FooterContainer, SocialIcon, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap, WebsiteRights } from './FooterElements'
 
 const Footer = () => {
     return(
@@ -9,6 +11,24 @@ const Footer = () => {
                         <SocialIcon />
                         EXPLOR
                     </SocialLogo>
+                    <WebsiteRights>EXPLOR copyright {new Date().getFullYear()}</WebsiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                            <Facebook />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                            <FaInstagram />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                            <FaYoutube />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+                            <FaTwitter />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                            <FaLinkedin />
+                        </SocialIconLink>
+                    </SocialIcons>
                 </SocialMediaWrap>
             </SocialMedia>
         </FooterContainer>
